1416ddf1fa575a715b40d4e840a3be15505c7cc5
Erick Randriamanantena <hajjaerick@gmail.com>
2024-07-04 15:16:03 -0400

    adding some changes

--- tests/test_models.py
+++ tests/test_models.py
@@ -23,6 +23,7 @@ While debugging just these tests it's convenient to use this:
     nosetests --stop tests/test_models.py:TestProductModel
 
 """
+
 import os
 import logging
 import unittest
@@ -36,16 +37,12 @@ DATABASE_URI = os.getenv(
 )
 
 
-######################################################################
-#  P R O D U C T   M O D E L   T E S T   C A S E S
-######################################################################
-# pylint: disable=too-many-public-methods
 class TestProductModel(unittest.TestCase):
     """Test Cases for Product Model"""
 
     @classmethod
     def setUpClass(cls):
-        """This runs once before the entire test suite"""
+        """Set up before the test suite runs"""
         app.config["TESTING"] = True
         app.config["DEBUG"] = False
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
@@ -54,24 +51,20 @@ class TestProductModel(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        """This runs once after the entire test suite"""
+        """Tear down after the test suite runs"""
         db.session.close()
 
     def setUp(self):
-        """This runs before each test"""
-        db.session.query(Product).delete()  # clean up the last tests
+        """Set up before each test case"""
+        db.session.query(Product).delete()  # Clean up the last tests
         db.session.commit()
 
     def tearDown(self):
-        """This runs after each test"""
+        """Tear down after each test case"""
         db.session.remove()
 
-    ######################################################################
-    #  T E S T   C A S E S
-    ######################################################################
-
     def test_create_a_product(self):
-        """It should Create a product and assert that it exists"""
+        """Test creating a product"""
         product = Product(name="Fedora", description="A red hat", price=12.50, available=True, category=Category.CLOTHS)
         self.assertEqual(str(product), "<Product Fedora id=[None]>")
         self.assertTrue(product is not None)
@@ -83,7 +76,7 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(product.category, Category.CLOTHS)
 
     def test_add_a_product(self):
-        """It should Create a product and add it to the database"""
+        """Test adding a product to the database"""
         products = Product.all()
         self.assertEqual(products, [])
         product = ProductFactory()
@@ -101,9 +94,8 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(new_product.available, product.available)
         self.assertEqual(new_product.category, product.category)
 
-    # TEST CASE
     def test_read_a_product(self):
-        """It should Read a Product"""
+        """Test reading a product from the database"""
         product = ProductFactory()
         product.id = None
         product.create()
@@ -116,12 +108,12 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found_product.price, product.price)
 
     def test_update_a_product(self):
-        """It should Update a Product"""
+        """Test updating a product in the database"""
         product = ProductFactory()
         product.id = None
         product.create()
         self.assertIsNotNone(product.id)
-        # Change it an save it
+        # Change it and save it
         product.description = "testing"
         original_id = product.id
         product.update()
@@ -133,18 +125,18 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(len(products), 1)
         self.assertEqual(products[0].id, original_id)
         self.assertEqual(products[0].description, "testing")
-    
+
     def test_delete_a_product(self):
-        """It should Delete a Product"""
+        """Test deleting a product from the database"""
         product = ProductFactory()
         product.create()
         self.assertEqual(len(Product.all()), 1)
-        # delete the product and make sure it isn't in the database
+        # Delete the product and make sure it isn't in the database
         product.delete()
         self.assertEqual(len(Product.all()), 0)
-    
+
     def test_list_all_products(self):
-        """It should List all Products in the database"""
+        """Test listing all products in the database"""
         products = Product.all()
         self.assertEqual(products, [])
         # Create 5 Products
@@ -156,37 +148,40 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(len(products), 5)
 
     def test_find_by_name(self):
-        """It should Find a Product by Name"""
+        """Test finding products by name"""
         products = ProductFactory.create_batch(5)
         for product in products:
             product.create()
         name = products[0].name
-        count = len([product for product in products if product.name == name])
+        count = len([prod for prod in products if prod.name == name])
         found = Product.find_by_name(name)
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.name, name)
 
     def test_find_by_availability(self):
-        """It should Find Products by Availability"""
+        """Test finding products by availability"""
         products = ProductFactory.create_batch(10)
         for product in products:
             product.create()
         available = products[0].available
-        count = len([product for product in products if product.available == available])
+        count = len([prod for prod in products if prod.available == available])
         found = Product.find_by_availability(available)
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.available, available)
 
     def test_find_by_category(self):
-        """It should Find Products by Category"""
+        """Test finding products by category"""
         products = ProductFactory.create_batch(10)
         for product in products:
             product.create()
         category = products[0].category
-        count = len([product for product in products if product.category == category])
+        count = len([prod for prod in products if prod.category == category])
         found = Product.find_by_category(category)
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.category, category)
+
+if __name__ == "__main__":
+    unittest.main()
